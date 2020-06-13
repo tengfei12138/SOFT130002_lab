c7665522a14b4c74a887c271b22732fbc9013f00
tengfei12138 <19302010046@fudan.edu.cn>
2020-06-13 15:33:27 +0800

    lab11

---
+++ lab11/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'login');
+define('DBUSER', 'root');
+define('DBPASS', 'Csf2002010');
+define('DBCONNSTRING','mysql:host=localhost;dbname=login');
+
+?>
